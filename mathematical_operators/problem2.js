ae259cf0a87f16a9488ff949c1224f36debf7a82
ARUNDAS P R <arundaspr1999@gmail.com>
2022-09-04 16:59:02 +0530

    mathematical operator problems

---
+++ mathematical_operators/problem2.js
@@ -0,0 +1,9 @@
+// Problem 2 : Double All
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5; 
+var sumOfDoubleAll = (one * 2) + (two * 2) + (three * 2) + (four * 2) + (five * 2) ;
+    
+console.log(sumOfDoubleAll) ;
